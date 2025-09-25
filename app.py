a0987a2a94a7b0e0102c502dc1b206a97b6e233e
wwongphunga-dev <w.wongphunga@gmail.com>
2025-09-25 16:27:09 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
- 
+
 import streamlit as st
 from ultralytics import YOLO
 from PIL import Image
 import numpy as np
- 
+
 st.title("YOLO Image Detection App :)")
- 
+
 # Load YOLO model
 model = YOLO("best.pt")
 # model = YOLO("runs/detect/train73/weights/best.pt")
 # model = YOLO("yolo11n.pt")
 # Upload image
 uploaded_image = st.file_uploader("Upload an image (jpg, png)", type=["jpg", "jpeg", "png"])
- 
+
 if uploaded_image is not None:
   # Show original image
   st.image(uploaded_image , caption="Uploaded Image", use_container_width=True)
@@ -27,3 +27,9 @@ if uploaded_image is not None:
   st.image(result_image , caption="YOLO Detection Result", use_container_width=True)
   st.success("Detection completed!")
   # Extract detection results
+  boxes = results[0].boxes
+  class_ids = boxes.cls.cpu().numpy().astype(int)
+  class_names = [model.names[i] for i in class_ids]
+  # Count animal
+  animal_count = class_names.count("animal")
+  st.write(f"Number of animal detected: **{animal_count}**")
